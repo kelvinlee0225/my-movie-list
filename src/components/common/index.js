38aebbed47a429c65bd9597dbeddcc231544f5ce
kelvinlee0225 <leekelvin0225@gmail.com>
2022-09-16 15:46:29 -0400

    Implement MoviesGridList in SearchScreen

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,4 @@
 export * from "./SearchBar";
 export * from "./FavoriteIcon";
 export * from "./RenderIf";
+export * from "./MoviesGridList";
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,9 @@
 import { useState } from "react";
-import {
-  FlatList,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { SearchBar } from "../components/common";
+import { StyleSheet } from "react-native";
+import { MoviesGridList, SearchBar } from "../components/common";
 import { LinearGradient } from "expo-linear-gradient";
 import { MoviesService } from "../api";
-import { Text } from "@rneui/themed";
 import { RenderIf } from "../components/common";
-import { IMAGE_BASE_URL } from "@env";
 
 export const SearchScreen = ({ navigation }) => {
   const movieService = new MoviesService();
@@ -33,36 +25,7 @@ export const SearchScreen = ({ navigation }) => {
         }}
       />
       <RenderIf condition={fetchedMovies.length > 0}>
-        <FlatList
-          data={fetchedMovies}
-          keyExtractor={(item) => item.id}
-          numColumns={2}
-          contentContainerStyle={styles.contentContainer}
-          renderItem={({ item }) => {
-            return (
-              <TouchableOpacity
-                style={styles.itemContainer}
-                onPress={() =>
-                  navigation.navigate("MovieDetail", {
-                    movieId: item.id,
-                    //These two last params are especially for FavoriteContext
-                    movieTitle: item.title,
-                    moviePoster: item.poster_path,
-                  })
-                }
-              >
-                <Image
-                  source={{
-                    uri: `${IMAGE_BASE_URL}/original/${item.poster_path}`,
-                  }}
-                  style={styles.image}
-                />
-                <Text style={styles.movieTitle}>{item.title}</Text>
-              </TouchableOpacity>
-            );
-          }}
-          ListFooterComponent={() => <View style={{ height: 20 }} />}
-        />
+        <MoviesGridList fetchedMovies={fetchedMovies} navigation={navigation} />
       </RenderIf>
     </LinearGradient>
   );
@@ -72,22 +35,5 @@ const styles = StyleSheet.create({
   background: {
     width: "100%",
     height: "100%",
-  },
-  contentContainer: {
-    marginBottom: 32,
-    marginTop: 16,
-    alignItems: "center",
-  },
-  itemContainer: {
-    padding: 8,
-    width: 180,
-  },
-  image: {
-    height: 185,
-    width: 160,
-  },
-  movieTitle: {
-    alignSelf: "center",
-    fontSize: 16,
   },
 });
