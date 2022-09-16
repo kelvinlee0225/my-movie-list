c627ae938253fc6767f35bc0f864de31e458feb3
kelvinlee0225 <leekelvin0225@gmail.com>
2022-09-15 22:31:38 -0400

    create basic movies list by implementing get movies by rating method

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,70 @@
-import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import { MoviesService } from "../api";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  MainTouchableImage,
+  PopularMoviesHeader,
+  PopularMoviesHorizontalList,
+  RenderIf,
+} from "../components";
+import { StyleSheet, View } from "react-native";
 
 export const HomeScreen = () => {
+  const movieService = new MoviesService();
+  const [movies, setMovies] = useState([]);
+  const [firstMovieWithPoster, setFirstMovieWithPoster] = useState({});
+
+  const getFirstMovieWithPoster = (fetchedMovies) => {
+    for (let movie of fetchedMovies) {
+      if (movie.poster_path) {
+        setFirstMovieWithPoster(movie);
+        return;
+      }
+    }
+    return false;
+  };
+
+  const getMovies = async () => {
+    const result = await movieService.getMoviesByRating();
+    const fetchedMovies = result.results;
+
+    if (fetchedMovies.length > 0) {
+      setMovies(fetchedMovies);
+
+      if (Object.keys(firstMovieWithPoster).length < 1)
+        getFirstMovieWithPoster(fetchedMovies);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <>
-      <Text>This is the Home Screen!</Text>
-    </>
+    <SafeAreaView>
+      {/* <RenderIf
+        condition={
+          movies.length > 0 && Object.keys(firstMovieWithPoster).length > 0
+        }
+      >
+        <MainTouchableImage firstMovieWithPoster={firstMovieWithPoster} />
+      </RenderIf> */}
+
+      <View style={styles.popularMoviesContainer}>
+        <PopularMoviesHeader title="Popular Movies in Theaters" />
+        <PopularMoviesHorizontalList movies={movies} />
+      </View>
+
+      <View style={styles.popularMoviesContainer}>
+        <PopularMoviesHeader title="Favorites" />
+        <PopularMoviesHorizontalList movies={movies} />
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  popularMoviesContainer: {
+    marginTop: 10,
+  },
+});
